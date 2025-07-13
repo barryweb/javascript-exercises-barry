eb6239006cd25d77f73ea6b653f7d56f5e08c154
besim <167048463+barryweb@users.noreply.github.com>
2025-07-13 20:23:01 +1000

    exercise-passed

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(arrayToString) {
+    return arrayToString
+        .split("")
+        .reverse()
+        .join("");
 };
 
 // Do not edit below this line
