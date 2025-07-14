d58f4762b404b4cf4206a685de5f93458ed7dc73
besim <167048463+barryweb@users.noreply.github.com>
2025-07-14 17:10:38 +1000

    exercise-passed

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let total = 0;
 
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if (Number.isNaN(num1) || Number.isNaN(num2)) return "ERROR";
+    if (typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
+
+    if (num1 > num2) {
+        for (let j = num1; j >= num2; j--) {
+            total += j;
+        }
+        return total;
+    }
+
+    for (let i = num1; i <= num2; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
