ca4c284f053dd5d912f0086a1e47ad6e431b7a84
besim <167048463+barryweb@users.noreply.github.com>
2025-07-14 19:05:36 +1000

    exercise-pass

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) / (9 / 5);
+  const rounded = Math.round(celsius * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    const fahrenheit = (celsius * 9 / 5) + 32;
+    const rounded = Math.round(fahrenheit * 10) / 10;
+    return rounded;
 };
 
 // Do not edit below this line
