d1219be986ac78384813ce6570e355404cf6b8e6
besim <167048463+barryweb@users.noreply.github.com>
2025-07-14 14:58:33 +1000

    exercise-passed

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
+//Spread operator... spreads the elements of an array
+//into individual elements. Used for copying, merging, or
+//passing to arguments.
+
+//Rest parameters... is used to collect multiple args
+//into a single array. Used when you dont know how many
+//arguments a function will take.
+
+const removeFromArray = function(array, ...remove) {
+    return array.filter((item) => !remove.includes(item));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
