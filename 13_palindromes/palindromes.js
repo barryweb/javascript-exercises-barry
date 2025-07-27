b019f011bab04160984a7a87a77fe07f78632c13
besim <167048463+barryweb@users.noreply.github.com>
2025-07-27 19:07:23 +1000

    exercise pass

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+	const cleanedString = str
+		.toLowerCase()
+		.split("")
+		.filter((character) => alphanumerical.includes(character))
+		.join("");
+
+	const reversedStr = cleanedString.split("").reverse().join("");
+
+	return cleanedString === reversedStr;
 };
 
 // Do not edit below this line
