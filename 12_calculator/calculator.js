fece8b9693d782177aeb6dcc6b0f447b280d913e
besim <167048463+barryweb@users.noreply.github.com>
2025-07-27 16:55:28 +1000

    exercise passed

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((total, num) => {
+		return total + num;
+	}, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+	return nums.reduce((sum, item) => {
+		return sum * item;
+	}, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let sum = 1;
+	for (let i = num; i > 1; i--) {
+		sum *= i;
+	}
+	return sum;
 };
 
 // Do not edit below this line
