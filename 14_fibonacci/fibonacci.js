267faa3aa1503ab2cdf3111ebd5a86eeab6b6d92
besim <167048463+barryweb@users.noreply.github.com>
2025-07-27 20:40:26 +1000

    exercise-passed

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -13,7 +13,7 @@ const fibonacci = function(num) {
 	let firstPrev = 1;
 	let secondPrev = 0;
 
-	for (let i = 2; i <= num; i++) {
+	for (let i = 2; i <= count; i++) {
 		let current = firstPrev + secondPrev;
 		secondPrev = firstPrev;
 		firstPrev = current;
